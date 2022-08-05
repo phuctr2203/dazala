90851c4871544b30d187c0e6a7a2df7250809f74
Felix Tran <104712325+phuctr2203@users.noreply.github.com>
2022-08-05 14:14:12 +0700

    Update dazala.sql

--- dazala.sql
+++ dazala.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE dazala;
-
+DROP DATABASE dazala;
 USE dazala;
 
 CREATE TABLE vendor_seq 
@@ -15,7 +15,7 @@ CREATE TABLE vendor
     latitude DECIMAL(10, 4) NOT NULL,
     longtitude DECIMAL(10, 4) NOT NULL,
     username VARCHAR(16) NOT NULL,
-    password VARCHAR(16) NOT NULL
+    password VARCHAR(255) NOT NULL
 );
 
 ALTER TABLE vendor 
@@ -32,7 +32,37 @@ END$$
 DELIMITER ;
 
 insert into vendor(name, address, latitude, longtitude, username, password)
-value ('aa', 'Ho Chi Minh City', 10, 10, 'phuc123', '123');
+value ('phuc', 'Ho Chi Minh City', 12, 10, 'phuc123', '123');
+
+CREATE TABLE product_seq 
+(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+);
+
+CREATE TABLE product
+(
+	id VARCHAR(10) NOT NULL PRIMARY KEY DEFAULT '0',
+	name VARCHAR(30) NOT NULL,
+	price DECIMAL(14, 2) NOT NULL,
+    ven_id VARCHAR(10) NOT NULL
+);
+
+ALTER TABLE product
+ADD CONSTRAINT FK_product_ven_id FOREIGN KEY (ven_id) REFERENCES vendor(id);
+    
+DELIMITER $$
+CREATE TRIGGER tg_product_insert
+BEFORE INSERT ON product
+FOR EACH ROW
+BEGIN
+  INSERT INTO product_seq VALUES (NULL);
+  SET NEW.id = CONCAT('PD', LPAD(LAST_INSERT_ID(), 3, '0'));
+END$$
+DELIMITER ;
+
+insert into product (name, price, ven_id) values ('iphone', 200, 'VD001');
+
+select v.name, p.name, p.price from vendor v join product p on v.id = p.ven_id ;
 
 
-select * from vendor;
+
