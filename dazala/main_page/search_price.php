e6786b46dd128b0042bd886e41b34dae4011dded
Felix Tran <104712325+phuctr2203@users.noreply.github.com>
2022-08-23 17:43:59 +0700

    Add files via upload

---
+++ dazala/main_page/search_price.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once '../login_page/db.php';
+
+session_start();
+
+$username = $_SESSION['username'];
+
+if(!isset($username)){
+    header('location:login.php');
+};
+
+?>
+
+<h1>Search by Price Page</h1>
+
+<section class="search-form">
+   <form action="" method="post">
+      <input type="float" name="start_price" placeholder="Start price" class="box" required>
+      <input type="float" name="end_price" placeholder="End price" class="box" required>
+      <input type="submit" name="submit" value="search" class="btn">
+   </form>
+</section>
+<button onclick='window.location.href="homepage.php";' class="btn btn-primary btn-block btn-flat" style="background-color: blue;">Back To Main Page</button> 
+
+<?php 
+    if(isset($_POST['submit'])) {
+        $start_price = $_POST['start_price'];
+        $end_price = $_POST['end_price'];   
+        $sql_search_price = "CALL search_product_based_on_price($start_price, $end_price);";
+        $rows_search_price = $customer->query($sql_search_price);
+
+        if($rows_search_price->rowCount() == 0) {
+            echo "<br>" . "No Results Found";
+        }
+
+        foreach($rows_search_price as $row) {
+            $id = $row['id'];
+            $name = $row['name'];
+            $price = $row['price'];
+            $quantity = $row['quantity'];
+            $ven_id = $row['ven_id'];
+
+            echo "<br>ID: " . $id. "<br>name: " . $name. "<br>price: $" . $price. "<br>quantity: " . $quantity. "<br>vendor id: " . $ven_id."";
+            echo "<br>";
+        }
+
+    }
