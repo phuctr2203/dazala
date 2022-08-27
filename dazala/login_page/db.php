c6ff1b543012f12f3efc96f4ff7b1e68883807b5
Felix Tran <104712325+phuctr2203@users.noreply.github.com>
2022-08-27 22:24:49 +0700

    Update db.php

--- dazala/login_page/db.php
+++ dazala/login_page/db.php
@@ -16,3 +16,14 @@ global $shipper;
 $vendor = new PDO('mysql:host=localhost;dbname=dazala', $ven_user, $ven_pass);
 $customer = new PDO('mysql:host=localhost;dbname=dazala', $cus_user, $cus_pass);
 $shipper = new PDO('mysql:host=localhost;dbname=dazala', $ship_user, $ship_pass);
+
+$sql_vendor = "SET DEFAULT ROLE vendor TO vendor@localhost;";
+$stmt_vendor = $vendor->query($sql_vendor);
+
+$sql_customer = "SET DEFAULT ROLE customer TO customer@localhost;";
+$stmt_customer = $customer->query($sql_customer);
+
+$sql_shipper= "SET DEFAULT ROLE shipper TO shipper@localhost;";
+$stmt_shipper = $shipper->query($sql_shipper);
+
+?>
