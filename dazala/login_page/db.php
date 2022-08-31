8330bafd34f6089a78dd3d4cdb7153c1efe284e5
Felix Tran <104712325+phuctr2203@users.noreply.github.com>
2022-09-01 01:32:14 +0700

    Update db.php

--- dazala/login_page/db.php
+++ dazala/login_page/db.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once("../vendor/autoload.php");
+
 $ven_user = 'vendor';
 $ven_pass = 'vendor';
 
@@ -12,10 +14,10 @@ $cus_pass = 'customer';
 $ship_user = 'shipper';
 $ship_pass = 'shipper';
 
-
 global $vendor;
 global $customer;
 global $shipper;
+global $collection;
 
 $vendor = new PDO('mysql:host=localhost;dbname=dazala', $ven_user, $ven_pass);
 $customer = new PDO('mysql:host=localhost;dbname=dazala', $cus_user, $cus_pass);
@@ -27,13 +29,16 @@ $link_ven = mysqli_connect('localhost', 'vendor', 'vendor', 'dazala');
 
 $link_ship = mysqli_connect('localhost', 'shipper', 'shipper', 'dazala');
 
-// $sql_vendor = "SET DEFAULT ROLE vendor";
-// $stmt_vendor = $vendor->query($sql_vendor);
+$sql_vendor = "SET DEFAULT ROLE vendor TO vendor@localhost";
+$stmt_vendor = $vendor->query($sql_vendor);
 
-// $sql_customer = "SET DEFAULT ROLE customer";
-// $stmt_customer = $customer->query($sql_customer);
+$sql_customer = "SET DEFAULT ROLE customer TO customer@localhost";
+$stmt_customer = $customer->query($sql_customer);
 
-// $sql_shipper= "SET DEFAULT ROLE shipper";
-// $stmt_shipper = $shipper->query($sql_shipper);
+$sql_shipper= "SET DEFAULT ROLE shipper TO shipper@localhost";
+$stmt_shipper = $shipper->query($sql_shipper);
+
+$client = new MongoDB\Client("mongodb://localhost:27017");
+$collection = $client->dazala->dazala;
 
 ?>
